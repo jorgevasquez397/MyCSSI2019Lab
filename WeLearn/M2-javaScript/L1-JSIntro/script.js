cf38d79d522d5544703953ab685d66fdcd0a33e5
Jorge Vasquez <vasqu182@csusm.edu>
2019-07-24 12:01:54 -0700

    javascript console

---
+++ WeLearn/M2-javaScript/L1-JSIntro/script.js
@@ -0,0 +1,15 @@
+console.log("script running!");
+
+let name = 'Charlie';
+console.log("Hello " + name + "!");
+let age = 20;
+console.log("You are " + age + " years old");
+
+if( age >= 16) {
+  console.log("You hella old m8")
+} else {
+  console.log("Go have fun young one!")
+}
+const function = () => {
+
+}
---
+++ WeLearn/M2-javaScript/L2-JSFunction/Console/script.js
@@ -0,0 +1,13 @@
+const greetPeople = (name1, name2) => {
+  console.log('hello, ' + name1 + ' and ' + name2 + '!');
+}
+const getFullName = (firstName, lastName) => {
+  return "Hello " + firstName + ' ' + lastName;
+}
+let radius = 10;
+let area = Math.PI * radius * radius;
+radius = radius * 2;
+area= Math.PI * Math.pow(radius, 2);
+let width = Math.round(Math.random() * 100);
+let height = Math.round(Math.random() * 50);
+let circumference = width * 2 + height * 2;
