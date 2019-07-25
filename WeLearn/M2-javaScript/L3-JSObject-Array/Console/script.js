c3727538464cee4b9e91a678f956652474a84c4a
Jorge Vasquez <vasqu182@csusm.edu>
2019-07-25 10:13:41 -0700

    array JS

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,25 @@
+const article = {
+  name: 'Dog family gives birth to 10 puppies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author: {
+    name: 'Joe Corgi',
+    title: 'Senior Canine Editor',
+  }
+};
+
+const movie = {
+  movie1: 'John Wick',
+  movie2: 'Avengers',
+  movie3: 'Annabelle',
+  movie4: 'The Lion King',
+  movie5: 'Detective Pikachu',
+};
+
+const food = {
+  food1: = 'Pizza',
+  food2: = 'Burrito',
+  food3: = 'Macaroni & cheese',
+  food4: = 'Hamburgers',
+  food5: = 'Watermelon',
+};
